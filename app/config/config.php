1d18aa7b364bb4d48e44874a52ecfa3ad283997c
Will Strong <wstrong@kellett.nt.ca>
2020-03-03 15:05:28 -0700

    Created global config file.

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//APPROOT:  Application Root Directory
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//URLROOT:  URL of Root
+define('URLROOT', 'https://traversymvn.lan');
+
+//SITENAME:
+define('SITENAME', 'TraversyMVC');
